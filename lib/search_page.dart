7b86d1d96e1014addaebe09e85ea2ce605596b3e
dongjunkun <1440418246@qq.com>
2018-07-02 17:08:27 +0800

    去掉搜框的边框，读取TextField 的值

--- lib/search_page.dart
+++ lib/search_page.dart
@@ -9,13 +9,21 @@ class SearchPage extends StatefulWidget {
 }
 
 class _SearchPageState extends State<SearchPage> {
+
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey();
+
+  final controller = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
+      key: _scaffoldKey,
       appBar: AppBar(
         title: TextField(
+          controller: controller,
           decoration: InputDecoration(
             hintText: '搜索真的好了，不骗你',
+            border: InputBorder.none,
             hintStyle: TextStyle(
               color: Colors.white.withAlpha(100)
             )
@@ -23,8 +31,21 @@ class _SearchPageState extends State<SearchPage> {
           style: TextStyle(
             color: Colors.white
           ),
+          onChanged: (text){
+            print(text);
+          },
         ),
+        actions: <Widget>[
+          new IconButton(icon: Icon(Icons.search), onPressed: (){
+            _scaffoldKey.currentState.showSnackBar(SnackBar(content:Text(controller.text) ));
+          })
+        ],
       ),
     );
+  }
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
   }
 }
